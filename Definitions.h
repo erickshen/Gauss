f0403d8473ea2a2bc757db7bfd28878cd802a433
erickshen <116446221+erickshen@users.noreply.github.com>
2023-06-27 13:59:37 +0300

    Add files via upload

---
+++ Definitions.h
@@ -0,0 +1,6 @@
+void ErrorNorm(double* x, double* y, int n);
+double MatrixNorm(double* x, int n);
+double FunctionInput(int k, int n, int i, int j);
+int MatrixInputFile(int m, double* x, char* t);
+void MatrixOutput(int l, int m, int n, double* x);
+int GaussMethod(double* massive, double* inverse, int*Column, double*Row, int n);
